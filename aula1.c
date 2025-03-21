e55b6febaf4abc57a8e2461134ad1083cf1c6860
MatiehusDev <mc172649@gmail.com>
2025-03-20 21:44:29 -0300

    Segundo

--- aula1.c
+++ aula1.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
- 
+int variavel1 = 100;
+
+void imprime();
 int main(){
-    printf("Hello World!");
+
+    printf("%d", variavel1);
+    void imprime();
+
     return 0;
+}
+void imprime(){
+    printf("%d", variavel1 + 10);
 }
